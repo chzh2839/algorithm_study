a351efa18c4e053a8cfea8c600e8470d735d7759
bora <59353414+chzh2839@users.noreply.github.com>
2025-02-11 22:27:14 +0900

    bfs

--- src/main/java/com/example/demo/algo1/Search.java
+++ src/main/java/com/example/demo/algo1/Search.java
@@ -11,10 +11,11 @@ import java.util.*;
  * */
 public class Search {
     public void doProcess() throws IOException {
-        getConnectedComponent();
+//        getConnectedComponent();
+        searchMaze();
     }
 
-    /** DFS */
+    /** DFS - 연결요소 구하기 */
     boolean[] visited; // 방문배열
     ArrayList<Integer>[] array; // 인접리스트
     private void getConnectedComponent() throws IOException {
@@ -54,6 +55,69 @@ public class Search {
         for (int i : array[nodeIndex]) {
             if (!visited[i]) { // 방문하지 않은 값이면
                 dfs(i);
+            }
+        }
+    }
+    /** BFS - 미로 탐색
+     * [
+     *  [0, 1, 0, 1, 1, 0],
+     *  [0, 0, 1, 0, 1, 1],
+     *  [0, 1, 0, 0, 0, 0],
+     *  [0, 1, 1, 1, 0, 0],
+     * ]
+     * 위 이차원 배열에서 (0,0)에서 마지막 (5,3)위치로 가는 횟수 구하기
+     * 단, 상하좌우로 1일 경우에만 이동 가능
+     * */
+    // 상하좌우를 탐색하기 위한 define값 정의 변수
+    // ex. index=0 => x축 0만큼 이동, y축 1만큼 이동은 배열에서 아래로 이동
+    // ex. index=1 => x축 1만큼 이동, y축 0만큼 이동은 배열에서 오른쪽으로 이동
+    int[] xMove = {0,1,0,-1};
+    int[] yMove = {1,0,-1,0};
+    boolean[][] visited2; // 방문배열
+    int[][] array2;
+    int widthCnt;
+    int heightCnt;
+    private void searchMaze() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        widthCnt = Integer.parseInt(st.nextToken());
+        heightCnt = Integer.parseInt(st.nextToken());
+        visited2 = new boolean[widthCnt][heightCnt];
+        // 인접리스트 구현
+        array2 = new int[widthCnt][heightCnt];
+        for (int w = 0; w < widthCnt; w++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken(); // ex. 110010
+            for (int h = 0; h < heightCnt; h++) {
+                array2[w][h] = Integer.parseInt(line.substring(h, h+1));
+            }
+        }
+
+        bfs(0,0); // 이차원배열 (0,0) 위치에서 시작
+        System.out.println("마지막 위치까지 간 횟수 : " + array2[widthCnt-1][heightCnt-1]);
+    }
+    private void bfs(int start, int end) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{start, end}); // offer()와 add()의 차이 참고 https://atomicliquors.tistory.com/26
+
+        System.out.println("queue 초기값 : " + queue.stream().map(Arrays::toString).toList());
+
+        // 큐가 빌때까지 반복
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            visited2[start][end] = true;
+            for (int i = 0; i < 4; i++) { // 상하좌우 탐색
+                int x = now[0] + xMove[i];
+                int y = now[1] + yMove[i];
+                if (x >= 0 && x < widthCnt && y >= 0 && y < heightCnt) {
+                    if (array2[x][y] != 0 && !visited2[x][y]) {
+                        System.out.printf("x로 %d만큼 y로 %d만큼 이동합니다.\n", xMove[i], yMove[i]);
+                        // 이동하려는 위치의 값이 0이 아니고, 방문하지 않았어야 함
+                        visited2[x][y] = true;
+                        array2[x][y] = array2[now[0]][now[1]] + 1; // 배열 값이 1뿐이니, 이동한 횟수를 여기에 1씩 더하면서 이동
+                        queue.add(new int[]{x, y});
+                    }
+                }
             }
         }
     }
