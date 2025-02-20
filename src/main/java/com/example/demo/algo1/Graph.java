afe8e747f97b461667021741e18352a30d7918e6
bora <59353414+chzh2839@users.noreply.github.com>
2025-02-20 22:04:31 +0900

    union find graph

--- src/main/java/com/example/demo/algo1/Graph.java
+++ src/main/java/com/example/demo/algo1/Graph.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  * Do it! 알고리즘 코딩테스트 with JAVA
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 public class Graph {
     public void doProcess() throws IOException {
 //        checkBipartiteGraph();
+        unionFind();
     }
 
     /** 이분 그래프 (bipartite graph) 판별하기
@@ -67,6 +69,57 @@ public class Graph {
             } else if (checkArr[start] == checkArr[i]) { // 인접한 두 개의 노드가 같은 집합이면, 사이클 존재, 이분그래프가 아니란 의미.
                 isBipartiteGraph = false;
             }
+        }
+    }
+
+    /** 유니온파인드 - 집합 표현하기
+     * 0 a b이면 a가 포함된 집합과 b가 포함된 집합 합치기 (union),
+     * 1 a b이면 두 원소가 같은 집합에 포함되어 있는지 확인 (find)
+     * */
+    int[] parent;
+    private void unionFind() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int nodeCnt = Integer.parseInt(st.nextToken());
+        int queryCnt = Integer.parseInt(st.nextToken());
+        parent = new int[nodeCnt + 1];
+        for (int i = 0; i < nodeCnt; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < queryCnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            int question = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (question == 0) {
+                union(a, b);
+            } else {
+                // 같은 집합 원소인지 확인
+                if (find(a) == find(b)) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+    // 각각의 대표노드를 찾아서, 다르면 두 원소의 대표노드끼리 연결하기.
+    private void union(int a, int b) {
+        int first = find(a);
+        int second = find(b);
+        if (first != second) {
+            parent[second] = first; // 두 개 연결
+        }
+    }
+    // 대표노드 찾기
+    private int find(int a) {
+        /* a가 대표노드면 리턴.
+        아니면 a의 대표노드값을 find(parent[a]) 값으로 저장 -> 재귀 함수 형태
+        * */
+        if (a == parent[a]) return a;
+        else {
+            return parent[a] = find(parent[a]); // **** value를 index로 바꿔서 또 찾기
         }
     }
 }
